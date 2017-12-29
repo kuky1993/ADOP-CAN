21786a7cb372b28a2ae0ba7ac2da365a4b37ddb5
JOSEOSTI <joseostaiza57@outlook.com>
2017-12-29 17:10:23 -0500

    cambios

--- insert.php
+++ insert.php
@@ -1,30 +1,23 @@
 <?php
 include('conect.php');
-$cedula=$_POST['cedula'];
-$nombres=$_POST['nombres'];
-$apellidos=$_POST['apellidos'];
-$apellidos=$_POST['direccion'];
-$apellidos=$_POST['correo'];
-$apellidos=$_POST['telefono'];
+$cedula=$_POST["cedula"];
+$nombres=$_POST["nombres"];
+$apellidos=$_POST["apellidos"];
+$apellidos=$_POST["direccion"];
+$apellidos=$_POST["correo"];
+$apellidos=$_POST["telefono"];
 
-$stmt = $DBcon->prepare("INSERT INTO Usuario(cedula,nombres,apellidos,direccion,correo,telefono) VALUES(:cedula, :nombres,:apellidos,:direccion,:correo,:telefono)");
-
-$stmt->bindparam(':cedula', $cedula);
-$stmt->bindparam(':nombres', $nombres);
-$stmt->bindparam(':apellidos', $apellidos);
-$stmt->bindparam(':direccion', $direccion);
-$stmt->bindparam(':correo', $correo);
-$stmt->bindparam(':telefono', $telefono);
-if($stmt->execute())
-{
-  $res="Data Inserted Successfully:";
-  echo json_encode($res);
+$insertar="INSERT INTO Usuario(cedula,nombres,apellidos,direccion,correo,telefono) VALUES('$cedula','$nombres','$apellidos','$direccion','$correo','$telefono')";
+//ejecute el insert
+$resultado=mysqli_query($conexion,$insertar)
+if(!$resultado){
+echo "error al ingresar datos";
 }
 else {
-  $error="Not Inserted,Some Probelm occur.";
-  echo json_encode($error);
+echo "datos ingresados exitosamente";
 }
-
+//cerrar coneccion
+mysqli_close($conexion)
 
 
  ?>
