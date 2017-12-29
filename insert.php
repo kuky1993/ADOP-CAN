17b7d7390af38bb66edf34234c2b10152b29b5e7
JOSEOSTI <joseostaiza57@outlook.com>
2017-12-29 17:30:45 -0500

    0

--- insert.php
+++ insert.php
@@ -3,18 +3,18 @@ include('conect.php');
 $cedula=$_POST["cedula"];
 $nombres=$_POST["nombres"];
 $apellidos=$_POST["apellidos"];
-$apellidos=$_POST["direccion"];
-$apellidos=$_POST["correo"];
-$apellidos=$_POST["telefono"];
+$direccion=$_POST["direccion"];
+$correo=$_POST["correo"];
+$telefono=$_POST["telefono"];
 
 $insertar="INSERT INTO Usuario(cedula,nombres,apellidos,direccion,correo,telefono) VALUES('$cedula','$nombres','$apellidos','$direccion','$correo','$telefono')";
 //ejecute el insert
 $resultado=mysqli_query($conexion,$insertar)
 if(!$resultado){
-echo "error al ingresar datos";
+echo 'error al ingresar datos';
 }
 else {
-echo "datos ingresados exitosamente";
+echo 'datos ingresados exitosamente';
 }
 //cerrar coneccion
 mysqli_close($conexion)
