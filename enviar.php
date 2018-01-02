31740cef5140059f7f1f2cc9826929913d7841e3
kuky1993 <rpozos@institutobenitojuarez.edu.ec>
2018-01-02 18:59:47 -0500

    prueba

--- enviar.php
+++ enviar.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 //Load composer's autoloader
-require 'vendor/autoload.php';
+require 'PHPMailer/autoload.php';
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
