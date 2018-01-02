c0db47d04e3c0175321284032a5588c8d1bdba19
JOSEOSTI <joseostaiza57@outlook.com>
2018-01-01 21:06:01 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-function insertData() {
-  var cedula=$("#cedula").val();
-  var nombres=$("#nombres").val();
-  var apellidos=$("#apellidos").val();
-  var direccion=$("#direccion").val();
-  var correo=$("#correo").val();
-  var telefono=$("#telefono").val();
 
-      $.ajax({
-          type: "POST",
-          url: "insert-/server/config/ConexionBaseDatos.php",
-          data: {cedula:cedula,nombres:nombres,apellidos:apellidos,direccion:direccion,correo:correo,telefono:telefono},
-          dataType: "JSON",
-          success: function(data) {
-           $("#message").html(data);
-          $("p").addClass("alert alert-success");
+  $( "#guardarDatos" ).submit(function( event ) {
+  var parametros = $(this).serialize();
+     $.ajax({
+        type: "POST",
+        url: "agregar.php",
+        data: parametros,
+         beforeSend: function(objeto){
+          $("#datos_ajax_register").html("Mensaje: Cargando...");
           },
-          error: function(err) {
-          alert(err);
-          }
-      });
+        success: function(datos){
+        $("#datos_ajax_register").html(datos);
 
-}
+        load(1);
+        }
+    });
+    event.preventDefault();
+  });
