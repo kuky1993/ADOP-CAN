2b390b065a33740c4ec8bb595490c72bb4501521
JOSEOSTI <joseostaiza57@outlook.com>
2018-01-06 01:33:46 -0500

    ca

--- animals/php/agregar.php
+++ animals/php/agregar.php
@@ -1,16 +1,16 @@
 <?php
 
 if(!empty($_POST)){
-	if(isset($_POST["id_animal"]) &&isset($_POST["nombre"]) &&isset($_POST["peso"]) &&isset($_POST["edad"]) &&isset($_POST["descripcion"])){
-		if($_POST["id_animal"]!=""&& $_POST["nombre"]!=""&&$_POST["peso"]!=""&&$_POST["edad"]!=""&&$_POST["descripcion"]!=""){
+		if(isset($_POST["id_animal"]) &&isset($_POST["nombre"]) &&isset($_POST["peso"]) &&isset($_POST["edad"]) &&isset($_POST["descripcion"])){
+			if($_POST["id_animal"]!=""&& $_POST["nombre"]!=""&&$_POST["peso"]!=""&&$_POST["edad"]!=""&&$_POST["descripcion"]!=""){
 			include "conexion.php";
 
-			$sql = "insert into Animal(id_animal,nombre,peso,edad,descripcion,created_at) value (\"$_POST[id_animal]\",\"$_POST[nombre]\",\"$_POST[peso]\",\"$_POST[edad]\",\"$_POST[descripcion]\",NOW())";
+			$sql = "update Animal set id_animal=\"$_POST[id_animal]\",nombre=\"$_POST[nombre]\",peso=\"$_POST[peso]\",edad=\"$_POST[edad]\",descripcion=\"$_POST[descripcion]\" where id=".$_POST["id"];
 			$query = $con->query($sql);
 			if($query!=null){
-				//print "<script>alert(\"Agregado exitosamente.\");window.location='../ver.php';</script>";
+				print "<script>alert(\"Actualizado exitosamente.\");window.location='../ver.php';</script>";
 			}else{
-				//print "<script>alert(\"No se pudo agregar.\");window.location='../ver.php';</script>";
+				print "<script>alert(\"No se pudo actualizar.\");window.location='../ver.php';</script>";
 
 			}
 		}
